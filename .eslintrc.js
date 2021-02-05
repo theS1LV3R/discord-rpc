1a94e408b97e0c737e2b6b238c40e49633f1fb11
S1LV3R <s1lv3r@corax.team>
2021-02-05 21:22:32 +0100

    fix: just fix shit. remove vuetify among other things

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,6 @@ module.exports = {
     "linebreak-style": ["error", "unix"],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "comma-dangle": ["warn", "always-multiline"]
+    "comma-dangle": ["warn", "always-multiline"],
   },
 };
--- src/background.ts
+++ src/background.ts
@@ -5,7 +5,7 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: "app", privileges: { secure: true, standard: true } }
+  { scheme: "app", privileges: { secure: true, standard: true } },
 ]);
 
 async function createWindow() {
@@ -17,8 +17,8 @@ async function createWindow() {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: (process.env
-        .ELECTRON_NODE_INTEGRATION as unknown) as boolean
-    }
+        .ELECTRON_NODE_INTEGRATION as unknown) as boolean,
+    },
   });
 
   if (process.env.WEBPACK_DEV_SERVER_URL) {
@@ -65,7 +65,7 @@ app.on("ready", async () => {
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === "win32") {
-    process.on("message", data => {
+    process.on("message", (data) => {
       if (data === "graceful-exit") {
         app.quit();
       }
